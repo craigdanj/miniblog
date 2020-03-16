857c857ea90facd86c9b73bcd4a70400da1c5912
Craig <craigdanj@gmail.com>
2020-03-17 01:16:34 +0530

    basic chat on client added

--- client/src/components/chat/index.js
+++ client/src/components/chat/index.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useMutation, useSubscription } from "@apollo/react-hooks";
 import './style.css';
 import gql from "graphql-tag";
-import { Spinner, Card, CardBody, Container, Row, Col, Button, Input } from 'reactstrap';
+import { Spinner, Card, CardBody, Container, Row, Col, Button, Input, Alert } from 'reactstrap';
 
 function FileUpload() {
 	const [token, setToken] = useState(null);
 	const [text, setText] = useState('');
+	const [chatList, setChatList] = useState([]);
 
 	const handleTextChange = e => {
 		setText(e.target.value);
@@ -43,11 +44,25 @@ function FileUpload() {
 		}
 	`;
 
-	const { data: commentData, loading: loadingComment } = useSubscription(COMMENTS_SUBSCRIPTION);
+	const { data: commentData, loading: loadingComment } = useSubscription(COMMENTS_SUBSCRIPTION, {
+		onSubscriptionData: ({subscriptionData}) => {
+			const newComment = subscriptionData.data.commentAdded.text;
+			setChatList([...chatList, newComment ])
+		}
+	});
 
 	return (
 		<div className="editPost">
 			<Container>
+				<Row>
+					<Col sm="12" md={{ size: 6, offset: 3 }}>
+						{chatList.map(text => (
+							<Alert color="primary">
+								{text}
+						  	</Alert>
+						))}
+					</Col>
+				</Row>
 				<Row>
 					<Col sm="12" md={{ size: 6, offset: 3 }}>
 						<h3>Chat:</h3>
@@ -58,8 +73,7 @@ function FileUpload() {
 								<Button color="primary" onClick={submitChat}>Submit</Button>
 								{loading && <Spinner type="grow" color="primary" />}
 							</CardBody>
-							{/* {data && data.addComment} */}
-							>>{commentData && commentData.commentAdded && commentData.commentAdded.text}
+
 						</Card>
 					</Col>
 				</Row>
